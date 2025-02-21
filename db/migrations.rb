f22955b7384e36496a0ebf86e501ade7d0f88617
igorFernandesBD <igorfernandestec@gmail.com>
2025-02-21 13:39:42 -0300

    Escopo da função main adicionada, ainda estruturando

--- db/migrations.rb
+++ db/migrations.rb
@@ -18,29 +18,59 @@ db = SQLite3::Database.new("./db/linktree.db")
 
 # create_database db
 
+def main(db) #Função main, não está funcionando corretamente, só tenho condições de consertar depois que tirar um cochilo
+  puts "[1] - Criar usuário  [2] - Login\n"
+  operacao = gets.chomp
 
-def insert_users(db) #Função para criação de usuário
-  
-  #Input do usuário para nome e senha
-  puts "Digite seu nome\n"
-  name_login = gets.chomp
-  puts "Digite sua senha\n"
-  password_login = gets.chomp
 
-  #Pega o ID para criação do username
-  id = db.execute("SELECT id FROM users ORDER BY id DESC LIMIT 1").flatten.first
-  idINT = id.to_i
-
-  #Inserção do usuário no banco e criação do username
-  db.transaction do  
-  db.execute("INSERT INTO users (name,username, password) VALUES (?, ?, ?)", [name_login, "#{name_login}.#{idINT}", password_login])
-  confirmacao = db.execute("SELECT * FROM users WHERE name = ? and password = ?", [name_login, password_login])
-  puts confirmacao
-  end
+  def insert_users(db) #Função para criação de usuário
     
-  
-end 
-insert_users db
+    #Captura o input do usuario
+    puts "Digite seu nome\n"
+    name_login = gets.chomp
+    puts "Digite sua senha\n"
+    password_login = gets.chomp  
 
+    #Pega o ID para criação do username
+    id = db.execute("SELECT id FROM users ORDER BY id DESC LIMIT 1").flatten.first
+    idINT = id.to_i
+
+    #Inserção do usuário no banco e criação do username
+    db.transaction do  
+    db.execute("INSERT INTO users (name,username, password) VALUES (?, ?, ?)", [name_login, "#{name_login}.#{idINT}", password_login])
+    end
+      
+    
+  end 
+  insert_users db
+
+  def login(db) #Função de login
+
+    #Captura o input do usuario
+    puts "Digite seu nome\n"
+    name_login = gets.chomp
+    puts "Digite sua senha\n"
+    password_login = gets.chomp
+    
+    begin
+    username = db.execute("SELECT username FROM users WHERE name = ? and password = ?", [name_login, password_login])
+    rescue 
+      puts"Usuario inexistente\n"
+    else 
+      puts("Bem vindo #{username}!")
+    end  
+  end
+
+  if operacao == 1
+
+    insert_users(db)
+
+  elsif operacao == 2
+    login(db)
+  end
+
+end
+
+main(db)
 
 db.close
